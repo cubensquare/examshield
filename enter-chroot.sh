0a05fd0a07cd49c4cacb9801c5027e12195a1ca4
cubensquare <66881155+cubensquarets@users.noreply.github.com>
2025-03-30 01:14:03 +0530

    Update enter-chroot.sh

--- enter-chroot.sh
+++ enter-chroot.sh
@@ -3,11 +3,12 @@
 # Set your working directory
 CHROOT_DIR=~/squashfs-root
 
+#dont use sudo in below mount commands if you have logged inas root user
 echo "🔧 Mounting system directories..."
-sudo mount --bind /dev "$CHROOT_DIR/dev"
-sudo mount --bind /dev/pts "$CHROOT_DIR/dev/pts"
-sudo mount --bind /proc "$CHROOT_DIR/proc"
-sudo mount --bind /sys "$CHROOT_DIR/sys"
+mount --bind /dev "$CHROOT_DIR/dev"
+mount --bind /dev/pts "$CHROOT_DIR/dev/pts"
+mount --bind /proc "$CHROOT_DIR/proc"
+mount --bind /sys "$CHROOT_DIR/sys"
 
 echo "✅ Mounted successfully. Entering chroot..."
-sudo chroot "$CHROOT_DIR" /bin/bash
+chroot "$CHROOT_DIR"
