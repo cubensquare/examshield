6e94ba579304f1e6ef68ac778b7b76cbd68ca7a8
cubensquare <66881155+cubensquarets@users.noreply.github.com>
2025-05-05 13:23:43 +0530

    Update new-send-post-rq.sh

--- new-send-post-rq.sh
+++ new-send-post-rq.sh
@@ -1,8 +1,27 @@
-curl -X POST http://<PXE_SERVER_IP>:5000/register \
+#!/bin/bash
+
+# Dynamically get the PXE server IP from default route
+PXE_IP=$(ip route | awk '/default/ {print $3}')
+
+# Validate IP
+if [[ -z "$PXE_IP" ]]; then
+  echo "[!] PXE_IP not found via default route."
+  exit 1
+fi
+
+# Extract interface and MAC address
+IFACE=$(ip -o link show | awk -F': ' '$2 !~ /^lo/ {print $2; exit}')
+MAC=$(cat /sys/class/net/"$IFACE"/address)
+IP=$(hostname -I | awk '{print $1}')
+HOSTNAME=$(hostname)
+STATUS="booted"
+
+# Send status to PXE server
+curl -X POST "http://$PXE_IP:5000/register" \
      -H "Content-Type: application/json" \
-     -d '{
-           "mac": "'$(cat /sys/class/net/eth0/address)'",
-           "ip": "'$(hostname -I | awk "{print \$1}")'",
-           "hostname": "'$(hostname)'",
-           "status": "booted"
-         }'
+     -d "{
+           \"mac\": \"$MAC\",
+           \"ip\": \"$IP\",
+           \"hostname\": \"$HOSTNAME\",
+           \"status\": \"$STATUS\"
+         }" || echo "[!] Failed to send PXE status"
