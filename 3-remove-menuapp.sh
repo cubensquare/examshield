d9719b70f7bd151170be17fa99d5735dd5521925
cubensquare <66881155+cubensquarets@users.noreply.github.com>
2025-05-18 18:05:18 +0530

    Create 3-remove-menuapp.sh

---
+++ 3-remove-menuapp.sh
@@ -0,0 +1,19 @@
+apt-get purge -y \
+  xfce4-appfinder \
+  catfish \
+  xarchiver \
+  xfce4-terminal \
+  mlterm \
+  mlterm-common \
+  xterm \
+  geany \
+  claws-mail \
+  xfce4-mailwatch-plugin \
+  debian-reference-* \
+  manpages-* \
+  doc-debian \
+  synaptic \
+  gnome-terminal \
+  gedit \
+  epiphany-browser \
+  mousepad
