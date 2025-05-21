d0c4799384389d8c2c61c774aeca85961a734f67
cubensquare <66881155+cubensquarets@users.noreply.github.com>
2025-05-22 00:18:33 +0530

    Update 6-disable-rightclick.sh

--- 6-disable-rightclick.sh
+++ 6-disable-rightclick.sh
@@ -1,1 +1,7 @@
-sudo -u goms xfconf-query -c xfce4-desktop -p /desktop-icons/context-menu -n -t bool -s false
+#!/bin/bash
+# disable-rightclick.sh
+
+# Disable right-click globally
+xinput --list | grep -i "mouse" | awk -F'id=' '{print $2}' | awk '{print $1}' | while read id; do
+  xinput set-button-map "$id" 1 0 3 4 5 6 7
+done
